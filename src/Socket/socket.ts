85be5f269cdb3a78c7f029e1369f6ec40ebfe951
Paulo Victor Lund <pvictorlv@gmail.com>
2025-09-12 13:16:09 -0300

    Revert "Update socket.ts"
    This reverts commit b57c61915e5770519ee3f14be45d8b966702c4c2.

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -135,7 +135,7 @@ export const makeSocket = (config: SocketConfig) => {
 	 * @param msgId the message tag to await
 	 * @param timeoutMs timeout after which the promise will reject
 	 */
-	const waitForMessage = async <T>(msgId: string, timeoutMs = defaultQueryTimeoutMs) => {
+	const waitForMessage = async <T>(msgId: string, timeoutMs = defaultQueryTimeoutMs) :Promise<any> => {
 		let onRecv: ((data: T) => void) | undefined
 		let onErr: ((err: Error) => void) | undefined
 		try {
@@ -187,9 +187,7 @@ export const makeSocket = (config: SocketConfig) => {
 
 		let [result] = await Promise.all([waitForMessage(msgId, timeoutMs), sendNode(node)])
 
-		// @ts-ignore
 		if (result && 'tag' in result) {
-			// @ts-ignore
 			assertNodeErrorFree(result)
 		}
 
